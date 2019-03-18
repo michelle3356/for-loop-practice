4eb4821fa9bafdf3e8cb6270b557b0ae77d402d2
michelle3356 <michelle3356@yahoo.com.tw>
2019-03-18 16:52:41 +0800

    1.js

---
+++ 1.js
@@ -0,0 +1,18 @@
+// 1.題目：求N! （如:5!=1*2*3*4*5）（必須用迴圈完成
+<script type="text/javascript">
+
+  function layer(num){
+    for(var i=num-1;i>=1;i--){
+      num = num *i;
+    }
+    return num;
+  }
+  console.log(layer(5));
+
+
+// 方法2
+//   function calc(n){
+//     if(n>0)return(calc(n-1)*n);
+//     return(1);
+// }
+// console.log(calc(5));
