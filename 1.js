ca03a39dc83453ca3fbb476b08e1b0bcd0e1c4d7
michelle3356 <michelle3356@yahoo.com.tw>
2019-03-18 21:34:09 +0800

    1jsdetail

--- 1.js
+++ 1.js
@@ -8,6 +8,16 @@
   console.log(layer(5));
 
 
+
+  // 5!=5*4*3*2*1
+  // num!=num*(num-1)*(num-2)*(num-3)*(num-4)
+  // 4!=4*3*2*1
+  // 3!=3*2*1
+  // 2!=2*1
+  // num!=num*(num-1)
+  // 1!=1*1
+
+
 // 方法2
 //   function calc(n){
 //     if(n>0)return(calc(n-1)*n);
