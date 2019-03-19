e9e6332faa2a03a529eb6dd46acdad06be136490
michelle3356 <michelle3356@yahoo.com.tw>
2019-03-19 19:16:11 +0800

    12js+13js

--- 12.js
+++ 12.js
@@ -1,6 +1,17 @@
 // 12.題目：輸入三個整數x,y,z，請把這三個數由小到大輸出。
+
+
+// 1.將數字指入一個陣列
+// 2.將陣列做升冪排列
+
+
+
 function toAsc(x, y, z) {
-  return [small, middle, big];
+  var A = [x, y, z];
+  // console.log(A.length);
+
+  A.sort((a,b) => a - b);
+  return A;
 }
 
 console.log(toAsc(5, 3, 4)); // [3, 4, 5]
